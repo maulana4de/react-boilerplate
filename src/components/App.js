36b25ca17e38cfa2c785c6dc961204e4e5811146
maulana <maulana4de@gmail.com>
2019-10-15 11:22:16 +0700

    BoilerPlate React Apps

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./App.scss";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <h3>Hello World</h3>
+        </div>
+        <div>
+          <p>lorem ipsum lorem ipsum lorem lorem lorem ipsum</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" }
+      },
+      {
+        test: /\.html$/,
+        use: { loader: "html-loader" }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
